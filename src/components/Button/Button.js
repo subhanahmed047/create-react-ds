5b06437301445b1f7e14c30207b5fcf543cc1693
Subhan Ahmed <subhanahmed047@gmail.com>
2021-01-09 17:06:37 +1100

    add styled components + add external dependencies

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,14 +1,26 @@
 import React from "react";
+import styled from "styled-components";
 
-const Button = (props) => {
-  return (
-    <button
-      style={{ color: "brown", backgroundColor: "lightgray" }}
-      onClick={() => alert("Clicked")}
-    >
-      {props.children}
-    </button>
-  );
+const StyledButton = styled.button`
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border-radius: 3px;
+
+  /* Color the border and text with theme.main */
+  color: ${(props) => props.theme.colors.primary};
+  border: 2px solid ${(props) => props.theme.colors.primary};
+`;
+
+// Passing a default theme for Buttons that arent wrapped in the ThemeProvider
+StyledButton.defaultProps = {
+  theme: {
+    colors: {
+      primary: "palevioletred",
+    },
+  },
 };
+
+const Button = (props) => <StyledButton>{props.children}</StyledButton>;
 
 export default Button;
--- webpack.config.js
+++ webpack.config.js
@@ -26,5 +26,7 @@ module.exports = {
   },
   externals: {
     react: "commonjs react",
+    "react-dom": "commonjs react-dom",
+    "styled-components": "commonjs styled-components",
   },
 };
